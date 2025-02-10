ff4fcb048b65ed397c6cd004929da4c68b9541ad
Ygor Alejandro <ygorhernadezbr@gmail.com>
2025-02-10 17:13:13 -0300

    Formating with masks

---
+++ formatmask.py
@@ -0,0 +1,9 @@
+codigo = 105
+nome = 'José Santana'
+salario = 1650.00
+status = True
+
+print('Código: %d'%codigo)
+print('Nome: %s'%nome)
+print('Salário: %.2f'%salario)
+print('Status: %r'%status)
