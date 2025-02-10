8cc63c5727ed3116d54feaa9165807b9d0e184a7
Ygor Alejandro <ygorhernadezbr@gmail.com>
2025-02-10 18:57:42 -0300

    DecisionStructure

---
+++ DecisionStructure.py
@@ -0,0 +1,25 @@
+#SimpleStructure
+
+A = input('Informe o valor para a variável A: ')
+B = input('Informe o valor para a variável B: ')
+
+if (A>B):
+    aux=A;
+    A=B;
+    B=aux;
+
+    print('O valor da variável A agora é : ', A);
+    print('O valor da variável B agora é : ', B);
+
+#CompoundStruture
+#Practice1
+
+idade = int(input('Digite a idade da pessoa: '))
+if idade > 18:
+    print('Maior Idade')
+else:
+    print('Menor Idade')
+
+
+
+
