0f3159b918e9aa37ea4c50fb3cc2e962b7fb4879
Ygor Alejandro <ygorhernadezbr@gmail.com>
2025-02-10 21:02:39 -0300

    Using conditional for

---
+++ for.py
@@ -0,0 +1,15 @@
+# Crecente
+for n in range(10):
+    print(n)
+
+#Determinando valor inicial
+print('')
+print('Determiando el valor inicial')
+for n in range(5, 16):
+    print(n)
+
+#Decrecente
+print('')
+print('Decrecente')
+for n in range(10, 0, -1):
+    print(n)
